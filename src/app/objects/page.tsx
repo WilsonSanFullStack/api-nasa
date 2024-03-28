703baba572ca259ee9288522b206cdbbf05d60a0
WilsonSanFullStack <harveysanch@gmail.com>
2024-03-28 05:07:43 -0500

    verificacion de cache

--- src/app/objects/page.tsx
+++ src/app/objects/page.tsx
@@ -49,21 +49,22 @@ async function getNeo() {
   }
 }
 export default async function page() {
-
   const data = await getNeo();
   const date1 = new Date(data.near_earth_objects[start_date()][0].close_approach_data[0].close_approach_date)
   // Obtener la fecha actual en UTC
-const currentDateUTC = new Date();
-// Convertir la fecha actual a UTC y obtener solo la fecha (sin horas ni minutos)
-const currentDayUTC = new Date(currentDateUTC).toISOString().slice(0, 10);
-// Obtener el día de date1 en formato UTC
-const dayDate1 = date1.toISOString().slice(0, 10);
-// Comparar si los días son iguales
-const sameDay = currentDayUTC === dayDate1;
-// if para revalidar la ruta objects
-if (!sameDay) {
+  const currentDateUTC = new Date('2024-03-30');
+  // Convertir la fecha actual a UTC y obtener solo la fecha (sin horas ni minutos)
+  const currentDayUTC = new Date(currentDateUTC).toISOString().slice(0, 10);
+  // Obtener el día de date1 en formato UTC
+  const dayDate1 = date1.toISOString().slice(0, 10);
+  // Comparar si los días son iguales
+  const sameDay = currentDayUTC === dayDate1;
+  // if para revalidar la ruta objects
+  if (!sameDay) {
+    console.log('me ejecuto')
   revalidatePath('/objects')
-}
+  }
+
 
   return (
     <main className="mt-12">
