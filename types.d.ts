c4eb0a61030dd590f106708e69ce9e6ffb196ec1
WilsonSanFullStack <harveysanch@gmail.com>
2024-03-29 23:28:07 -0500

    arreglo errores

---
+++ types.d.ts
@@ -0,0 +1,86 @@
+// Interface for UTCTime
+declare interface UTCTime {
+  date: string | undefined;
+  time: string | undefined;
+}
+declare type LocalTimeOffset = {
+  minutes: number | undefined;
+};
+
+// Interface for LocalTimeOffset
+declare interface LocalTimeOffset {
+  minutes: number | undefined;
+}
+
+// **New:** Interface for API response error
+declare interface ApiError {
+  error: {
+    message: string | undefined;
+    code: number | undefined;
+  };
+}
+
+// **Combined:** Interface for NEODetails (combines best aspects of both responses)
+declare interface NEODetails {
+  id: string | undefined;
+  name: string | undefined;
+  nasa_jpl_url: string | undefined;
+  absolute_magnitude_h: number | undefined;
+  estimated_diameter: {
+    kilometers: {
+      estimated_diameter_min: number | undefined;
+      estimated_diameter_max: number | undefined;
+    };
+  };
+  is_potentially_hazardous_asteroid: boolean | undefined;
+  close_approach_data: CloseApproachData[];
+  is_sentry_object: boolean | undefined;
+}
+
+// Interface for CloseApproachData (unchanged)
+declare interface CloseApproachData {
+  close_approach_date: string | undefined;
+  close_approach_date_full: string | undefined;
+  epoch_date_close_approach: number | undefined;
+  relative_velocity: {
+    kilometers_per_second: number | undefined;
+    kilometers_per_hour: number | undefined;
+  };
+  miss_distance: {
+    kilometers: number | undefined;
+    lunar_distances: number | undefined;
+  };
+  orbiting_body: string | undefined;
+}
+
+// Interface for NEOsByDate (unchanged)
+declare interface NEOsByDate {
+  [id: string | undefined]: NEODetails;
+}
+
+// Interface for NEO (unchanged)
+declare interface NEO {
+  links: {
+    self: string | undefined;
+    next: string | undefined;
+    prev: string | undefined;
+  };
+  element_count: number | undefined;
+  near_earth_objects: {
+    [date: string | undefined]: NEOsByDate;
+  };
+}
+
+declare interface DATOS {
+  datos: NEO
+}
+
+declare interface IMAGE {
+  date: string | undefined;
+  explanation: string | undefined;
+  hdurl: string | undefined;
+  media_type: string | undefined;
+  service_virsion: string | undefined;
+  title: string | undefined;
+  url: string | undefined;
+}
